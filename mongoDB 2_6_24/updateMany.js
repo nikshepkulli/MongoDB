2197835582b4ab040d1005b5b54b52be1e55149e
Nikshep A Kulli <48430792+nikshepkulli@users.noreply.github.com>
2024-02-07 12:04:22 -0500

    Update updateMany.js

--- mongoDB 2_6_24/updateMany.js
+++ mongoDB 2_6_24/updateMany.js
@@ -1,4 +1,4 @@
 db.books.updateMany(
-  {publishedDate: {$1t: ISODate("2017-04-27T08:00:00.000+00:00")}},
+  {publishedDate: {$lt: ISODate("2017-04-27T08:00:00.000+00:00")}},
   {$set: {status: "NEW"}}
 );
